4db3b5f52a1745db6dd9c1d424bdd6778c26aa89
chaechae66 <valueworkerchae@gmail.com>
2023-12-14 13:25:58 +0900

    Feat: intro page 구현 진행 중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 "use client";
 
-import { FlexBox } from "@/global.style";
+import { BannerImageWrap } from "@/style/page.style";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <FlexBox width="100%" height="100vh">
-      <button>
-        <a
-          href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}`}
-        >
-          카카오톡 로그인
-        </a>
-      </button>
-    </FlexBox>
+    <>
+      <BannerImageWrap>
+        {/* <Image src={"/bannerBg.jpg"} alt="모임" fill /> */}
+      </BannerImageWrap>
+    </>
   );
 }
---
+++ src/style/page.style.ts
@@ -0,0 +1,7 @@
+import { styled } from "styled-components";
+
+export const BannerImageWrap = styled.div`
+  width: 100%;
+  background-color: #f00;
+  height: 400px;
+`;
