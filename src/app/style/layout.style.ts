e0cd269c7ad79c79a57fd8b6f590acd62beeaacf
chaechae66 <valueworkerchae@gmail.com>
2023-12-24 14:04:41 +0900

    Design: 글로벌 스타일링 수정

--- src/app/style/layout.style.ts
+++ src/app/style/layout.style.ts
@@ -1,19 +1,23 @@
-import { FlexBox } from "@/style/global.style";
+import { FlexBox } from "@/app/style/global.style";
 import { styled } from "styled-components";
 
 export const Header = styled.header`
   width: 100%;
   height: 60px;
-  background-color: #674bf2;
+  background-color: #fff;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 0 20px;
+  border-bottom: 0.5px solid #f1f1f1;
+  position: sticky;
+  z-index: 2;
+  top: 0;
 `;
 
 export const Logo = styled.h1`
   & > a {
-    color: #fff;
+    color: #6038ff;
     font-size: 22px;
     font-weight: 500;
   }
@@ -21,17 +25,19 @@ export const Logo = styled.h1`
 
 export const LoginBtn = styled.button`
   & > a {
-    color: #fff;
+    color: #333;
   }
-  background-color: #ffffff60;
+  background-color: #ffe402;
   box-sizing: content-box;
   padding: 10px;
 `;
 
 export const Footer = styled(FlexBox)`
+  margin-top: 100px;
   background-color: #f9f9f9;
 `;
 
 export const Content = styled.section`
   min-height: calc(100vh - 120px);
+  padding: 0 50px;
 `;
--- src/style/page.style.ts
+++
@@ -1,7 +0,0 @@
-import { styled } from "styled-components";
-
-export const BannerImageWrap = styled.div`
-  width: 100%;
-  background-color: #f00;
-  height: 400px;
-`;
