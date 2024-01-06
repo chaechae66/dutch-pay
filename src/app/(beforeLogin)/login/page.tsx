38d396593a5c17660bf5b64abfcead36c752da3b
chaechae66 <valueworkerchae@gmail.com>
2024-01-06 15:25:37 +0900

    Chore: 로그인 페이지 생성

---
+++ src/app/(beforeLogin)/login/page.tsx
@@ -0,0 +1,10 @@
+export default function Login() {
+  return (
+    <>
+      {/* <a
+  href={`https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_REST_API_KEY}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}`}
+> */}
+      로그인
+    </>
+  );
+}
